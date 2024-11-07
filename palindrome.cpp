b33afdedec8d5eb6777f8f5d39d5274dc174524f
SURJEET KUMAR PARJAPATI <118541540+surjeetprj@users.noreply.github.com>
2024-11-07 16:35:30 +0530

    Add files via upload

--- palindrome.cpp
+++ palindrome.cpp
@@ -8,12 +8,12 @@ string longest_palindromic_subsequence(const string& S) {
     vector<vector<string>> dp(n, vector<string>(n, ""));
     
     // Base case
-    for (int i = 0; i < n; i++) {
+    for (int i=0; i<n; i++) {
         dp[i][i] = S[i];
     }
     
-    
-    for (int L = 2; L <= n; L++) {
+
+    for (int L=2; L<=n; L++) {
         for (int i = 0; i <= n-L; i++) {
             int j = i+L-1;
             if (S[i] == S[j]) {
